31e8705251e4d25d343ea83295d94b2696046be4
Linse Mathew <linsemathew123@gmail.com>
2016-09-03 23:13:52 -0400

    Add ball and paddles.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,32 @@
-var name = prompt("What is your name?")
+// var name = prompt("What is your name?")
 
 var canvas;
 var canvasContext;
+var xCoordinate = 50;
 
 window.onload = function(){
 	canvas = document.getElementById('gameCanvas');
 	canvasContext = canvas.getContext('2d');
-	canvasContext.fillStyle = 'darkblue';
+	var framesPerSecond = 30;
+	setInterval(moveBallAndDrawOnCanvas, 1000 / framesPerSecond);
+}
+
+function moveBallAndDrawOnCanvas(){
+	moveBall();
+	drawOnCanvas();
+}
+
+function moveBall(){
+	xCoordinate += 5
+}
+
+function drawOnCanvas(){
+	canvasContext.fillStyle = '#0E0024';
 	canvasContext.fillRect(0,0,canvas.width,canvas.height);
+	canvasContext.fillStyle = 'white';
+	canvasContext.fillRect(5,100, 10, 75);
+	canvasContext.fillStyle = 'while';
+	canvasContext.fillRect(785, 100, 10, 75);
+	canvasContext.fillStyle = 'red';
+	canvasContext.fillRect(xCoordinate,100,10,10);
 }
