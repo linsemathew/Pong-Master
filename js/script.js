840d12d06c273bdb940697808f040d3a24434f3f
Linse Mathew <linsemathew123@gmail.com>
2016-09-05 02:29:16 -0400

    Add mousePosition function.

--- js/script.js
+++ js/script.js
@@ -2,13 +2,30 @@
 
 var canvas;
 var canvasContext;
-var xCoordinate = 50;
-var backgroundColor = '#0E0024'
+var ballXCoordinate = 395;
+var ballXSpeed = 5;
+var ballYCoordinate = 395;
+var ballYSpeed = 5;
+var ballRadius = 10;
+var leftPlayerPlayerY = 250;
+const PADDLE_HEIGHT = 85;
+
+function mousePosition(event){
+	var rect = canvas.getBoundingClientRect();
+	var root = document.documentElement;
+	var mouseX = event.clientX - rect.left - root.scrollLeft;
+	var mouseY = event.clientY - rect.top - root.scrollTop;
+	return {
+		x: mouseX,
+		y: mouseY
+	};
+};
 
 window.onload = function(){
 	canvas = document.getElementById('gameCanvas');
 	canvasContext = canvas.getContext('2d');
 	var framesPerSecond = 30;
+
 	setInterval(function(){
 		moveBall();
 		drawOnCanvas();
@@ -16,16 +33,42 @@ window.onload = function(){
 }
 
 function moveBall(){
-	xCoordinate += 5
+	ballXCoordinate += ballXSpeed;
+	ballXSpeed += 0.01;
+	ballYCoordinate += ballYSpeed;
+	ballYSpeed += 0.01;
+
+	if (ballXCoordinate >= (canvas.width - 5) || ballXCoordinate <= (ballRadius / 2)){
+		ballXSpeed *= -1
+	}
+
+	if (ballYCoordinate >= (canvas.height - 5) || ballYCoordinate <= (ballRadius / 2)){
+		ballYSpeed *= -1
+	}
 }
 
 function drawOnCanvas(){
-	canvasContext.fillStyle = backgroundColor;
-	canvasContext.fillRect(0,0,canvas.width,canvas.height);
-	canvasContext.fillStyle = 'white';
-	canvasContext.fillRect(5,100, 10, 75);
-	canvasContext.fillStyle = 'while';
-	canvasContext.fillRect(785, 100, 10, 75);
-	canvasContext.fillStyle = 'red';
-	canvasContext.fillRect(xCoordinate,100,10,10);
+	// play area
+	makeRect(0, 0, canvas.width, canvas.height, '#0E0024');
+
+	// left player paddle
+	makeRect(5, leftPlayerPlayerY, 10, PADDLE_HEIGHT, 'white');
+
+	// right player paddle
+	makeRect((canvas.width-15), 250, 10, PADDLE_HEIGHT, 'white');
+
+	// ball
+	makeBall(ballXCoordinate, ballYCoordinate, ballRadius, 'red')
+}
+
+function makeBall(xPosition, yPosition, radius, color){
+	canvasContext.fillStyle = color;
+	canvasContext.beginPath();
+	canvasContext.arc(xPosition, yPosition, radius, 0, Math.PI*2, true)
+	canvasContext.fill();
+}
+
+function makeRect(xPosition, yPosition, width, height, color){
+	canvasContext.fillStyle = color
+	canvasContext.fillRect(xPosition, yPosition, width, height)
 }
