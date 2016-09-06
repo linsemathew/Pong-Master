f1f004ae68ff360df82350c41e32ed95f49c6cd7
Linse Mathew <linsemathew123@gmail.com>
2016-09-06 04:10:01 -0400

    Make paddle width a constant.

--- js/script.js
+++ js/script.js
@@ -2,13 +2,14 @@
 
 var canvas;
 var canvasContext;
-var ballXCoordinate = 395;
+var ballXCoordinate;
 var ballXSpeed = 5;
-var ballYCoordinate = 395;
+var ballYCoordinate;
 var ballYSpeed = 5;
 var ballRadius = 10;
 var leftPlayerPlayerY = 250;
 const PADDLE_HEIGHT = 85;
+const PADDLE_WIDTH = 10;
 
 function mousePosition(event){
 	var rect = canvas.getBoundingClientRect();
@@ -25,6 +26,9 @@ function mousePosition(event){
 window.onload = function(){
 	canvas = document.getElementById('gameCanvas');
 	canvasContext = canvas.getContext('2d');
+	ballXCoordinate = canvas.width / 2;
+	ballYCoordinate = canvas.height / 2;
+
 	var framesPerSecond = 30;
 
 	setInterval(function(){
@@ -40,18 +44,22 @@ window.onload = function(){
 };
 
 function moveBall(){
-	ballXCoordinate += ballXSpeed;
+	ballXCoordinate -= ballXSpeed;
 	ballXSpeed += 0.01;
-	ballYCoordinate += ballYSpeed;
+	ballYCoordinate -= ballYSpeed;
 	ballYSpeed += 0.01;
 
-	if (ballXCoordinate >= (canvas.width - 5) || ballXCoordinate <= (ballRadius / 2)){
-		ballXSpeed *= -1
-	}
+	if (ballYCoordinate < (leftPlayerPlayerY + PADDLE_HEIGHT) && ballYCoordinate > leftPlayerPlayerY && ballXCoordinate <= (5 +PADDLE_WIDTH + ballRadius){
+		ballXSpeed *= -1;
+	} else if (ballXCoordinate >= (canvas.width - ballRadius){
+		resetBallPosition();
+	} else if (ballXCoordinate <= ballRadius){
+		resetBallPosition();
+	};
 
-	if (ballYCoordinate >= (canvas.height - 5) || ballYCoordinate <= (ballRadius / 2)){
+	if (ballYCoordinate >= (canvas.height - ballRadius) || ballYCoordinate <= ballRadius){
 		ballYSpeed *= -1
-	}
+	};
 }
 
 function drawOnCanvas(){
@@ -59,23 +67,29 @@ function drawOnCanvas(){
 	makeRect(0, 0, canvas.width, canvas.height, '#0E0024');
 
 	// left player paddle
-	makeRect(5, leftPlayerPlayerY, 10, PADDLE_HEIGHT, 'white');
+	makeRect(5, leftPlayerPlayerY, PADDLE_WIDTH, PADDLE_HEIGHT, 'white');
 
 	// right player paddle
-	makeRect((canvas.width-15), 250, 10, PADDLE_HEIGHT, 'white');
+	makeRect((canvas.width - PADDLE_WIDTH - 5), rightPlayerY, PADDLE_WIDTH, PADDLE_HEIGHT, 'white');
 
 	// ball
-	makeBall(ballXCoordinate, ballYCoordinate, ballRadius, 'red')
+	makeBall(ballXCoordinate, ballYCoordinate, ballRadius, 'red');
 }
 
 function makeBall(xPosition, yPosition, radius, color){
 	canvasContext.fillStyle = color;
 	canvasContext.beginPath();
-	canvasContext.arc(xPosition, yPosition, radius, 0, Math.PI*2, true)
+	canvasContext.arc(xPosition, yPosition, radius, 0, Math.PI*2, true);
 	canvasContext.fill();
 }
 
 function makeRect(xPosition, yPosition, width, height, color){
-	canvasContext.fillStyle = color
-	canvasContext.fillRect(xPosition, yPosition, width, height)
+	canvasContext.fillStyle = color;
+	canvasContext.fillRect(xPosition, yPosition, width, height);
+}
+
+function resetBallPosition(){
+	ballYCoordinate = canvas.height / 2;
+	ballXCoordinate = canvas.width / 2;
+	ballXSpeed *= -1;
 }
